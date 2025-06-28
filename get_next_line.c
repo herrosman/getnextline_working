978636ab57b0ba127cc9a4bc8ce9605568f127c3
Abdi Osman <aosman@c2r10s4.42wolfsburg.de>
2025-06-28 16:56:45 +0200

    final working copy to submit

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aosman <aosman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 19:25:22 by aosman            #+#    #+#             */
-/*   Updated: 2025/06/27 20:23:29 by aosman           ###   ########.fr       */
+/*   Updated: 2025/06/28 16:52:25 by aosman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,47 +27,47 @@ static char	*ft_check_newline(const char *str, int c)
 	return (NULL);
 }
 
-static char	*backup_r(int fd, char *backup)
+static char	*save_backup(char *backup)
 {
-	ssize_t	bytes_r;
-	char	*buffer;
+	char	*res;
+	int		i;
+	int		j;
 
-	buffer = malloc(BUFFER_SIZE + 1);
-	if (!buffer)
-		return (NULL);
-	bytes_r = 1;
-	while (!ft_check_newline(backup, '\n') && bytes_r > 0)
+	i = 0;
+	while (backup[i] && backup[i] != '\n')
+		i++;
+	if (!backup[i])
 	{
-		bytes_r = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_r < 0)
-		{
-			free(buffer);
-			free(backup);
-			return (NULL);
-		}
-		buffer[bytes_r] = '\0';
-		backup = ft_strjoin(backup, buffer);
-		if (!backup)
-		{
-			free(buffer);
-			return (NULL);
-		}
+		free(backup);
+		return (NULL);
 	}
-	free(buffer);
-	return (backup);
+	res = malloc(ft_strlen(backup) - i);
+	if (!res)
+		return (NULL);
+	i++;
+	j = 0;
+	while (backup[i])
+		res[j++] = backup[i++];
+	res[j] = '\0';
+	free(backup);
+	return (res);
 }
 
 static char	*extract_line(char *backup)
 {
 	char	*line;
-	int		i;
+	size_t	i;
+	size_t	len;
 
 	if (!backup || backup[0] == '\0')
 		return (NULL);
+	len = 1;
 	i = 0;
 	while (backup[i] && backup[i] != '\n')
 		i++;
-	line = malloc(i + 2);
+	if (backup[i] == '\n')
+		len = 2;
+	line = malloc(len + i);
 	if (!line)
 		return (NULL);
 	i = 0;
@@ -82,30 +82,33 @@ static char	*extract_line(char *backup)
 	return (line);
 }
 
-static char	*save_remaining(char *backup)
+static char	*backup_r(int fd, char *backup)
 {
-	char	*res;
-	int		i;
-	int		j;
+	ssize_t	bytes_r;
+	char	*buffer;
 
-	i = 0;
-	while (backup[i] && backup[i] != '\n')
-		i++;
-	if (!backup[i])
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes_r = 1;
+	while (!ft_check_newline(backup, '\n') && bytes_r > 0)
 	{
-		free(backup);
-		return (NULL);
+		bytes_r = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_r < 0)
+		{
+			free(backup);
+			return (NULL);
+		}
+		buffer[bytes_r] = '\0';
+		backup = ft_strjoin(backup, buffer);
+		if (!backup)
+		{
+			free(buffer);
+			return (NULL);
+		}
 	}
-	res = malloc(ft_strlen(backup) + 1 - i);
-	if (!res)
-		return (NULL);
-	i++;
-	j = 0;
-	while (backup[i])
-		res[j++] = backup[i++];
-	res[j] = '\0';
-	free(backup);
-	return (res);
+	free(buffer);
+	return (backup);
 }
 
 char	*get_next_line(int fd)
@@ -127,6 +130,6 @@ char	*get_next_line(int fd)
 		return (NULL);
 	}
 	line = extract_line(backup);
-	backup = save_remaining(backup);
+	backup = save_backup(backup);
 	return (line);
 }
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aosman <aosman@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 12:52:45 by aosman            #+#    #+#             */
-/*   Updated: 2025/06/27 20:06:44 by aosman           ###   ########.fr       */
+/*   Updated: 2025/06/28 16:54:00 by aosman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,42 +27,47 @@ static char	*ft_check_newline(const char *str, int c)
 	return (NULL);
 }
 
-static char	*backup_r(int fd, char *backup)
+static char	*save_backup(char *backup)
 {
-	ssize_t	bytes_r;
-	char	*buffer;
+	char	*res;
+	int		i;
+	int		j;
 
-	buffer = malloc(BUFFER_SIZE + 1);
-	if (!buffer)
-		return (NULL);
-	bytes_r = 1;
-	while (!ft_check_newline(backup, '\n') && bytes_r > 0)
+	i = 0;
+	while (backup[i] && backup[i] != '\n')
+		i++;
+	if (!backup[i])
 	{
-		bytes_r = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_r < 0)
-		{
-			free(buffer);
-			free(backup);
-			return (NULL);
-		}
-		buffer[bytes_r] = '\0';
-		backup = ft_strjoin(backup, buffer);
+		free(backup);
+		return (NULL);
 	}
-	free(buffer);
-	return (backup);
+	res = malloc(ft_strlen(backup) - i);
+	if (!res)
+		return (NULL);
+	i++;
+	j = 0;
+	while (backup[i])
+		res[j++] = backup[i++];
+	res[j] = '\0';
+	free(backup);
+	return (res);
 }
 
 static char	*extract_line(char *backup)
 {
 	char	*line;
-	int		i;
+	size_t	i;
+	size_t	len;
 
-	if (!backup || !*backup)
+	if (!backup || backup[0] == '\0')
 		return (NULL);
+	len = 1;
 	i = 0;
 	while (backup[i] && backup[i] != '\n')
 		i++;
-	line = malloc(i + 2);
+	if (backup[i] == '\n')
+		len = 2;
+	line = malloc(len + i);
 	if (!line)
 		return (NULL);
 	i = 0;
@@ -77,30 +82,33 @@ static char	*extract_line(char *backup)
 	return (line);
 }
 
-static char	*save_remaining(char *backup)
+static char	*backup_r(int fd, char *backup)
 {
-	char	*res;
-	int		i;
-	int		j;
+	ssize_t	bytes_r;
+	char	*buffer;
 
-	i = 0;
-	while (backup[i] && backup[i] != '\n')
-		i++;
-	if (!backup[i])
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes_r = 1;
+	while (!ft_check_newline(backup, '\n') && bytes_r > 0)
 	{
-		free(backup);
-		return (NULL);
+		bytes_r = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_r < 0)
+		{
+			free(backup);
+			return (NULL);
+		}
+		buffer[bytes_r] = '\0';
+		backup = ft_strjoin(backup, buffer);
+		if (!backup)
+		{
+			free(buffer);
+			return (NULL);
+		}
 	}
-	res = malloc(ft_strlen(backup) + 1 - i);
-	if (!res)
-		return (NULL);
-	i++;
-	j = 0;
-	while (backup[i])
-		res[j++] = backup[i++];
-	res[j] = '\0';
-	free(backup);
-	return (res);
+	free(buffer);
+	return (backup);
 }
 
 char	*get_next_line(int fd)
@@ -115,9 +123,13 @@ char	*get_next_line(int fd)
 		return (NULL);
 	}
 	backup[fd] = backup_r(fd, backup[fd]);
-	if (!backup[fd])
+	if (!backup[fd] || *backup[fd] == '\0')
+	{
+		free(backup[fd]);
+		backup[fd] = NULL;
 		return (NULL);
+	}
 	line = extract_line(backup[fd]);
-	backup[fd] = save_remaining(backup[fd]);
+	backup[fd] = save_backup(backup[fd]);
 	return (line);
 }
